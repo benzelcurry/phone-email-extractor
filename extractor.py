0e6da0c932255effc205ce99a1f0569fc37288a0
Benzel <benawhiting@gmail.com>
2024-10-06 14:54:09 -0700

    Convert groups to non-capturing groups

--- extractor.py
+++ extractor.py
@@ -1,14 +1,20 @@
 import pyperclip, re
 
-phoneRegex = re.compile(r'''(
-    (\d{3}|\(\d{3}\))?              # area code
-    (\s|-|.)?                       # separator
+phone_regex = re.compile(r'''(
+    (?:\d{3}|\(\d{3}\))?              # area code
+    (?:\s|-|.)?                       # separator
     \d{3}                           # first 3 digits
-    (\s|-|.)                        # separator
+    (?:\s|-|.)                        # separator
     \d{4}                           # last 4 digits
-    (\s*(ext|x|ext.)\s*\d{2,5})?    # extension
+    (?:\s*(ext|x|ext.)\s*\d{2,5})?    # extension
     )''', re.VERBOSE)
 
-emailRegex = re.compile(r'[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}')
+email_regex = re.compile(r'[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}')
 
 page_contents = pyperclip.paste()
+
+phone_numbers = phone_regex.findall(page_contents)
+email_addresses = email_regex.findall(page_contents)
+
+print(phone_numbers)
+print(email_addresses)
